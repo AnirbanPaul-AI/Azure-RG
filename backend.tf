544d4d35e3de7a53f0ebfb3a1af488274b07edf3
Anirban Paul <anirban.paul07@gmail.com>
2026-06-12 23:40:05 +0530

    terraform remote backend

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate12345678"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+
+}
---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "demo" {
+
+  name     = "rg-demo-github-actions"
+  location = "East US2"
+
+}
