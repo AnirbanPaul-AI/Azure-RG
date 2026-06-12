024d6018b92fcae0247a9f2e132f5b087f630982
Anirban Paul <anirban.paul07@gmail.com>
2026-06-13 00:35:01 +0530

    version3

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstate12345678"
     container_name       = "tfstate"
+    key                  = "windows-vm-prod.tfstate"
   }
 
 }
