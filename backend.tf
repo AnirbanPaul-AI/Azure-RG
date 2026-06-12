e4c3e704ad495dc8aea802a8f794541b27a4a8e7
Anirban Paul <anirban.paul07@gmail.com>
2026-06-13 00:18:04 +0530

    version1

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate12345678"
+    container_name       = "tfstate"
+
+    key = "windows-vm-prod.tfstate"
+  }
+
+}
